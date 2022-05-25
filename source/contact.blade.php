4a37c98871a8e4ad059cd20c529f3032dc121ffc
PANDAD <daijianpu@gmail.com>
2022-05-25 16:29:33 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -1,28 +1,28 @@
 @extends('_layouts.master')
 
-@section('title', 'Contact')
+@section('title', '联系')
 
 @section('content')
-    <h1>Contact</h1>
+    <h1>联系</h1>
 
     <form action="https://formcarry.com/s/{{ $page->services->formcarry }}" method="post">
         <div>
-            <label for="sender">Name</label><br>
+            <label for="sender">姓名</label><br>
             <input type="text" name="sender" id="sender" required>
         </div>
 
         <div>
-            <label for="email">Email</label><br>
+            <label for="email">邮箱</label><br>
             <input type="email" name="email" id="email" required>
         </div>
 
         <div>
-            <label for="message">Message</label><br>
+            <label for="message">内容</label><br>
             <textarea name="message" id="message" required></textarea>
         </div>
 
         <input type="text" name="_gotcha" style="display: none;">
 
-        <input type="submit" name="submit" value="Send">
+        <input type="submit" name="submit" value="发送">
     </form>
 @endsection
