31a52f39a65836917dafbe5747eff96f0fb8d6d5
PANDAD <daijianpu@gmail.com>
2022-05-25 16:19:11 +0800

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -1,13 +1,13 @@
 @extends('_layouts.master')
 
-@section('title', 'About')
+@section('title', '关于')
 
 @section('content')
-    <h1>About</h1>
+    <h1>关于</h1>
 
-    <p>My name is {{ $page->owner->name }}</p>
+    <p>樱庭雅树</p>
 
-    <h2>Links:</h2>
+    <h2>链接:</h2>
 
     <ul>
         <li><a href="/twitter" target="_blank">Twitter</a></li>
