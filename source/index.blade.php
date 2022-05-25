7186cc8ccfe9b5674bcb5cf43819a56f4ae6fd24
PANDAD <daijianpu@gmail.com>
2022-05-25 16:04:45 +0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,17 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>你好，我正在测试。</h1>
+    <h1>我正在测试</h1>
 
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
+    <p>11111</p>
 
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
+    <p>222222</p>
 
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
+    <p>3333333</p>
 
+    <h2 id="test">444444</h2>
+
+    <p>666666</p>
+
+    <p>点击右边<a href="#test" class="test-js">联系我</a></p>
+@endsection
