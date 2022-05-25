218293332d6d77fb9b3f4093780fde88eb4ee3b4
PANDAD <daijianpu@gmail.com>
2022-05-25 16:39:47 +0800

    Update favicon.blade.php

--- source/_partials/head/favicon.blade.php
+++ source/_partials/head/favicon.blade.php
@@ -1,5 +1,5 @@
 <!-- favicon -->
 {{-- try out https://realfavicongenerator.net --}}
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01gYwxPj1xeOUy1nkr9_!!2710706468.jpg">
+<link rel="icon" type="image/png" sizes="16x16" href="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01gYwxPj1xeOUy1nkr9_!!2710706468.jpg">
 <!-- end favicon -->
