6018304f9669d9e62cfd990f797148548da1321d
PANDAD <daijianpu@gmail.com>
2022-05-26 16:09:00 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -4,9 +4,5 @@
 
 @section('content')
     <h1>联系</h1>
-<form action="https://formcarry.com/s/v9yUD0L9szr" method="POST">
-	<label for="email">填写你的邮箱</label>
-	<input type="email" name="email" id="email">
-	<button type="submit">等我回复你</button>
-</form>
+<a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">给我写信</a>
 @endsection
