d2555f14eda636ccca07565d20abecf46664d387
PANDAD <daijianpu@gmail.com>
2022-05-26 16:41:38 +0800

    Update post.blade.php

--- source/_layouts/post.blade.php
+++ source/_layouts/post.blade.php
@@ -18,12 +18,12 @@
     </p>
 
     <blockquote data-phpdate="{{ $page->date }}">
-        <em>WARNING: This post is over a year old. Some of the information this contains may be outdated.</em>
+        <em></em>
     </blockquote>
 
     <hr>
 
-    <p>DISCLAIMER: Any 3rd-party services in these posts and in the config are only recommendations/suggestions. I am not affiliated with any of them.</p>
+    <p></p>
 
     @yield('postContent')
 
@@ -34,6 +34,6 @@
     @if ($page->comments)
         @include('_partials.comments')
     @else
-        <p>Comments are not enabled for this post.</p>
+        <p></p>
     @endif
 @endsection
