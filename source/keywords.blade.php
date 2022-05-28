123d65e5746d8a836e276a035ba31c63bc022695
PANDAD <daijianpu@gmail.com>
2022-05-28 10:46:50 +0800

    Update keywords.blade.php

--- source/keywords.blade.php
+++ source/keywords.blade.php
@@ -3,7 +3,7 @@
 @section('title', '分类')
 
 @section('content')
-    <h1>关键词</h1>
+    <h1>分类</h1>
  <ul>
   <li><a href="/twitter" target="_blank">感悟</a></li>
   <li><a href="/github" target="_blank">技术</a></li>
