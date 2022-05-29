f490ec02b68e41644cc38348e75479fa26189412
PANDAD <daijianpu@gmail.com>
2022-05-29 10:49:09 +0800

    Update tag.blade.php

--- source/_layouts/tag.blade.php
+++ source/_layouts/tag.blade.php
@@ -9,7 +9,7 @@
         @forelse ($posts->filter->hasTag($page->name()) as $post)
             <li>
                 <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
-                <small>{{ $post->prettyDate() }}</small>
+                <small>{{ $post->prettyDate('Y-m-d') }}</small>
             </li>
         @empty
             <p>暂时没有相关文章！</p>
