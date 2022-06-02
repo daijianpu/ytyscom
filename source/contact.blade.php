4322ff56c7080ad74b4136adda19d61afd47b459
PANDAD <daijianpu@gmail.com>
2022-06-02 11:02:19 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -4,8 +4,10 @@
 
 @section('content')
     <h1>联系</h1>
-    请点击下面链接向我发送邮件！
+    我有1个苹果，你有1个苹果，交换之后，每人还是1个苹果。<br>
+    你有1个观点，我有1个观点，讨论之后，每人就有2个观点。<br>
+   看了我的文章，你有更好的观点，请点下面链接与我友好谈论！<br>
  <ul>
-        <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送邮件</a></li>
+ <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送留言</a></li>
     </ul>
 @endsection
