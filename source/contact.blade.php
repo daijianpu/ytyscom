53c3a59705845325ae59e4595ec4319098d9abfe
PANDAD <daijianpu@gmail.com>
2022-06-02 09:38:29 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
     <h1>联系</h1>
-    请点击下面向我发送邮件！不加QQ。
+    请点击下面链接向我发送邮件！
  <ul>
-        <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">给我写信</a></li>
+        <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送邮件</a></li>
     </ul>
 @endsection
