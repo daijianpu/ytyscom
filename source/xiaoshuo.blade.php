af6f70ab3e5b21a7d185946720bd229c6ebd9cb8
PANDAD <daijianpu@gmail.com>
2022-08-02 11:35:53 +0800

    Update and rename xiaoshuo to xiaoshuo.blade.php

---
+++ source/xiaoshuo.blade.php
@@ -0,0 +1,14 @@
+@extends('_layouts.master')
+
+@section('title', '小说')
+
+@section('content')
+<h1>小说</h1>
+<h2>目前在写ing...</h2>
+<li>《异星逃生》</li>
+<li>《AI觉醒危机》</li>
+可以去起点网搜我的小说。
+ <ul>
+ <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送留言</a></li>
+    </ul>
+@endsection
