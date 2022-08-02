7c47deff54001e6f691b7194696acef44d7aceab
PANDAD <daijianpu@gmail.com>
2022-08-02 11:39:54 +0800

    Update xiaoshuo.blade.php

--- source/xiaoshuo.blade.php
+++ source/xiaoshuo.blade.php
@@ -7,7 +7,7 @@
 <h2>正在写ing...未完</h2>
 <li>《异星逃生》</li>
 <li>《AI觉醒危机》</li>
-如果感兴趣去起点网搜我的小说！
+如果感兴趣对话，到起点网搜以上书名！
  <ul>
  <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送留言</a></li>
     </ul>
