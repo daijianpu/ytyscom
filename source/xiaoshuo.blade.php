41434aa16a1db6902f0b232608fe4ea8aee0e478
PANDAD <daijianpu@gmail.com>
2022-08-16 10:00:04 +0800

    Update xiaoshuo.blade.php

--- source/xiaoshuo.blade.php
+++ source/xiaoshuo.blade.php
@@ -3,10 +3,11 @@
 @section('title', '四百八十四')
 
 @section('content')
-<h1>四百八十四</h1>
+<h1>南昌四百八十四，几篇小说在知乎。</h1>
 <h2>瞎写ing...</h2>
 <li>《写完再告诉你们书名1》</li>
 <li>《写完再告诉你们书名2》</li>
+<li>《写完再告诉你们书名3》</li>
  <ul>
  <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送留言</a></li>
     </ul>
