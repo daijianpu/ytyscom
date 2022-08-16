da0535c01b8376e067305b7d77c42c5a51affe44
PANDAD <daijianpu@gmail.com>
2022-08-16 10:21:53 +0800

    Update keywords.blade.php

--- source/keywords.blade.php
+++ source/keywords.blade.php
@@ -7,9 +7,11 @@
  <ul>
   <li><a href="https://www.yingtingyashu.com/tags/%E6%84%9F%E6%82%9F" >感悟</a></li>
   <li><a href="https://www.yingtingyashu.com/tags/%E6%95%99%E8%82%B2" >教育</a></li>
+  <li><a href="https://www.sibaibashisi.com/tags/%E5%B0%8F%E8%AF%B4" >小说</a></li>
   <li><a href="https://www.yingtingyashu.com/tags/%E6%8A%80%E6%9C%AF" >技术</a></li>
   <li><a href="https://www.yingtingyashu.com/tags/%E5%9B%BD%E9%99%85" >国际</a></li>
   <li><a href="https://www.yingtingyashu.com/tags/%E7%BB%8F%E5%95%86" >经商</a></li>
+ 
   <li><a href="https://www.yingtingyashu.com/posts/" >全部</a></li>
     </ul>
 @endsection
