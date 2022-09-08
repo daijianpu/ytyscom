4e5d6c8e212cd30b3a1083ecc1b3f921a3eeaaf9
PANDAD <daijianpu@gmail.com>
2022-09-08 11:07:44 +0800

    Update xiaoshuo.blade.php

--- source/xiaoshuo.blade.php
+++ source/xiaoshuo.blade.php
@@ -5,10 +5,11 @@
 @section('content')
 <h1>小说</h1>
 <h2>南昌四百八十四，几篇小说在知乎。</h2>
-<li><a href="/posts/tzyy" >桃之妖妖</a></li>
+<li><a href="/posts/tzyy" >《桃之妖妖》</a></li>
 <li>《豆腐西尸》</li>
 <li>《人工智能路西法》</li>
  <ul>
  <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送留言</a></li>
     </ul>
 @endsection
+
