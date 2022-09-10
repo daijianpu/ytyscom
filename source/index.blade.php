524805f2e7bfa21d32507d49777454664f751bf0
PANDAD <daijianpu@gmail.com>
2022-09-10 10:35:23 +0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,5 +4,5 @@
     <a href="/">
   <img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01HifUHA1xeOWeSEkUP_!!2710706468.jpg" />
 </a>
-“敢有犯汉者，虽远必诛之！”<br>
+“为天地立心，为生民立命，为往圣继绝学，为万世开太平”。<br>
 @endsection
