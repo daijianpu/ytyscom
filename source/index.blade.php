7c2176f4f3a94a2625f685c5d011cf7ebdf9bfd7
PANDAD <daijianpu@gmail.com>
2022-09-12 10:05:46 +0800

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,5 +4,5 @@
     <a href="/">
   <img src="https://img.alicdn.com/imgextra/i4/2710706468/O1CN01HifUHA1xeOWeSEkUP_!!2710706468.jpg" />
 </a>
-“为天地立心，为生民立命，为往圣继绝学，为万世开太平”。<br>
+“生如蝼蚁，当有鸿鹄之志，命如纸薄，应有不屈之心。为天地立心，为生民立命，为往圣继绝学，为万世开太平。”<br>
 @endsection
