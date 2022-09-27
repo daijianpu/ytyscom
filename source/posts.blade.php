c01c7c95794b2f6e61e3c498cd1f6365561f19b4
PANDAD <daijianpu@gmail.com>
2022-09-27 12:06:52 +0800

    Update posts.blade.php

--- source/posts.blade.php
+++ source/posts.blade.php
@@ -4,13 +4,14 @@
 
 @section('content')
     <h1>文章</h1>
-
+<hr style="border-color: silver"/>
     <ul>
         @forelse ($posts->sortBy('title', 'desc') as $post)
             <li>
                 <a href="{{ $post->getPath() }}">{{ $post->title }}</a>
                 <small>{{ $post->prettyDate('Y-m-d') }}</small>
             </li>
+            <hr style="border-color: silver"/>
         @empty
             <p>暂时没有文章，稍后再来！</p>
         @endforelse
