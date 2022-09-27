f48c78726d1f2e7b59da56c7fdf82a435f32f636
PANDAD <daijianpu@gmail.com>
2022-09-27 12:09:02 +0800

    Update contact.blade.php

--- source/contact.blade.php
+++ source/contact.blade.php
@@ -4,10 +4,12 @@
 
 @section('content')
     <h1>联系</h1>
+    <hr style="border-color: silver"/>
     我有1个苹果，你有1个苹果，交换之后，每人还是1个苹果。<br>
     你有1个观点，我有1个观点，讨论之后，每人就有2个观点。<br>
     你骂我1句，我回骂你1句，拉黑之后，每人都少了一个朋友。<br>
    看了我的文章，你有更好的观点，请点下面链接与我友好交流！<br>
+   <hr style="border-color: silver"/>
  <ul>
  <li><a target="_blank" href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" style="text-decoration:none;">向我发送留言</a></li>
     </ul>
