78174f57ddceca4ef6a9207f4e95206d837347c9
PANDAD <daijianpu@gmail.com>
2022-09-27 12:09:53 +0800

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -4,24 +4,30 @@
 
 @section('content')
 <h1>关于</h1>
+<hr style="border-color: silver"/>
 <a href="/contact">
   <img src="https://img.alicdn.com/imgextra/i3/2710706468/O1CN01Skzhqo1xeOV7WjdgY_!!2710706468.jpg" width="150"/>
 </a>
 <h2>四百八十四</h2>
+<hr style="border-color: silver"/>
 这是我的笔名，小时候梦想是成为作家。事与愿违，家里穷只得选读国际贸易，毕业后一直做生意。<br>
 前几年断断续续写博客写文章，由于主机空间不稳定，辛苦码字却忘记续费被删掉了，没地方留存。<br>
 近期我公司做外贸需要建立网站，再次鼓捣虚拟主机，顺手把博客开起来，将来写了文章就来更新。<br>
 <h2>樱庭雅树</h2>
+<hr style="border-color: silver"/>
 我的主业是与日本客户做国际贸易，由于公司名简写时，有一个汉字在日语中没有对应的字。<br>
 收发货物时缺这个字，不好辨认造成损失，启用“樱庭雅树”作为收件名以弥补公司名的不足。<br>
 另一个原因：我喜欢吃樱桃，老了回家乡，盖一个院子，庭院里要种满樱桃树，吃个够！<br>
 <h2>友好讨论</h2>
+<hr style="border-color: silver"/>
 我的文章及观点，受制于我的认知不足，如果你不认同，肯定是你说的对。<br>
 你我素昧平生，没有必要与一个永不见面的人吵架结仇，看不惯关掉就行。<br>
 <h2>在线留言</h2>
+<hr style="border-color: silver"/>
 你可以给我发邮件，我每天工作都是打开邮箱的。<br>
 能随时看到你的留言，不友好的言论我不作回复。<br>
+<hr style="border-color: silver"/>
 <ul>
-<li><a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" target="_blank">向我发送留言</a></li>
-    </ul>
+<h2><li><a href="http://mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=Tn9-e3t6end3Dj8-YC0hIw" target="_blank">向我发送留言</a></li>
+    </ul></h2>
 @endsection
